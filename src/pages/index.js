041232041f7ab6c650a499433e9fb07c211756b3
elorapeter <florenceonyi09@gmail.com>
2025-07-16 11:39:05 +0000

    Jul 16, 2025, 12:39 PM

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ const { toggleWishlist, isInWishlist } = useWishlist();
 
   return (
 
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-100 pb-10">
   <Navbar />
   <div className="max-w-7xl mx-auto">
     <h1 className="text-4xl font-extrabold text-center mb-12 text-purple-700">
