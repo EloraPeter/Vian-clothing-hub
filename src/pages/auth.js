43262b996b7c895fc361ccaf09b6a07429c5bddc
elorapeter <florenceonyi09@gmail.com>
2025-07-23 11:39:55 +0000

    Jul 23, 2025, 12:39 PM

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -75,9 +75,8 @@ export default function Auth() {
       <Link href="/" className="flex items-center space-x-2 p-6">
         <img src="/logo.svg" alt="Aunty Nwanne Logo" className="h-12 w-auto" />
       </Link>
-      <div className="min-h-screen ml-50 p-4">
+      <div className="min-h-screen flex  ml-50 p-4">
         <div className="relative bg-white rounded-2xl shadow-2xl p-8 w-full max-w-md transform transition-all duration-500 hover:scale-105 animate-fade-in">
-          <div className="absolute inset-0 bg-gray-900/30 rounded-2xl -z-10" />
           <form onSubmit={handleSubmit} className="space-y-6">
             <h1 className="text-3xl font-extrabold text-center text-purple-800 font-['Playfair_Display']">
               {mode === 'login' ? 'Welcome Back' : 'Join the Style'}
