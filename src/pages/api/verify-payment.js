1a2c92023a720c7b2b00a740d501d8aa91c8f68a
elorapeter <florenceonyi09@gmail.com>
2025-07-25 14:46:36 +0000

    Jul 25, 2025, 3:46 PM

--- src/pages/api/verify-payment.js
+++ src/pages/api/verify-payment.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   // Handle CORS preflight
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.vianclothinghub.com.ng');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Max-Age', '86400');
