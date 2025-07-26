732c26b0e1225ed43253f4d10329f6db321671b1
elorapeter <florenceonyi09@gmail.com>
2025-07-26 17:20:41 +0000

    Jul 26, 2025, 6:20 PM

---
+++ src/lib/colorMap.js
@@ -0,0 +1,11 @@
+export const colorMap = {
+  Red: '#FF0000',
+  Blue: '#0000FF',
+  Green: '#008000',
+  Black: '#000000',
+  White: '#FFFFFF',
+  Yellow: '#FFFF00',
+  Purple: '#800080',
+  Pink: '#FF69B4',
+  // Add more colors as needed
+};
