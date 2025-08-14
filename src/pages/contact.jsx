2d5ef4d234429de9866af0d2dd2d6589a4e7f56c
elorapeter <florenceonyi09@gmail.com>
2025-08-14 16:32:48 +0000

    Aug 14, 2025, 5:32 PM

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -275,16 +275,17 @@ export default function Contact({ profile }) {
                             </div>
                             <div className="bg-white rounded-xl shadow-md p-6 md:p-8">
                                 <h2 className="text-2xl font-semibold text-purple-700 mb-6">Our Location</h2>
-                                <iframe
-                                    src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.249749709716!2d7.500898275995539!3d9.040967888799447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0beca8cf9473%3A0xe0d27cf270cbda9d!2sGimbiya%20St%2C%20Garki%2C%20Abuja%20900103%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1755188792480!5m2!1sen!2sng"></iframe>"
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.249749709716!2d7.500898275995539!3d9.040967888799447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e0beca8cf9473%3A0xe0d27cf270cbda9d!2sGimbiya%20St%2C%20Garki%2C%20Abuja%20900103%2C%20Federal%20Capital%20Territory!5e0!3m2!1sen!2sng!4v1755188792480!5m2!1sen!2sng"
                                     width="100%"
                                     height="250"
                                     style={{ border: 0 }}
-                                    allowFullScreen=""
+                                    allowfullscreen=""
                                     loading="lazy"
                                     title="Vian Clothing Hub Location"
                                     className="rounded-lg"
                                 ></iframe>
+
+
                             </div>
                             <div className="text-center">
                                 <Link href="/faqs" className="text-purple-600 hover:underline font-semibold">
